cb3548d51aeab020b51e1942731e433b57082afb
Jason <sanchezjason@me.com>
2025-04-22 18:37:15 -0500

    BankAccountTests

--- BluBank/BluBank/Models/BankAccount.swift
+++ BluBank/BluBank/Models/BankAccount.swift
@@ -7,4 +7,22 @@
 
 import Foundation
 
-// BankAccount class
+enum DepositType {
+    case check
+    case cash
+    case transfer
+}
+
+class BankAccount {
+    var accountNumber: String
+    private(set) var balance: Double
+    
+    init(accountNumber: String, balance: Double) {
+        self.accountNumber = accountNumber
+        self.balance = balance
+    }
+    
+    func deposit(amount: Double, depositType: DepositType) {
+        self.balance += amount
+    }
+}
--- BluBank/BluBankTests/BankAccountTests.swift
+++ BluBank/BluBankTests/BankAccountTests.swift
@@ -6,7 +6,14 @@
 //
 
 import Testing
+@testable import BluBank
 
 struct BankAccountTests {
-
+    @Test(arguments: [DepositType.cash, DepositType.check])
+    func deposit_amount_using_check_or_cash_increases_balance(_ depositType: DepositType) async throws {
+        let bankAccount = BankAccount(accountNumber: "123456", balance: 500)
+        bankAccount.deposit(amount: 200, depositType: depositType)
+        
+        #expect(bankAccount.balance == 700)
+    }
 }
