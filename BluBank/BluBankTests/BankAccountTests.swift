1bb2b50458642a792f00bbe8849f462b4c052b15
Jason <sanchezjason@me.com>
2025-04-22 18:40:32 -0500

    arrange, act, assert

--- BluBank/BluBankTests/BankAccountTests.swift
+++ BluBank/BluBankTests/BankAccountTests.swift
@@ -11,9 +11,11 @@ import Testing
 struct BankAccountTests {
     @Test(arguments: [DepositType.cash, DepositType.check])
     func deposit_amount_using_check_or_cash_increases_balance(_ depositType: DepositType) async throws {
+        // ARRANGE
         let bankAccount = BankAccount(accountNumber: "123456", balance: 500)
+        // ACT
         bankAccount.deposit(amount: 200, depositType: depositType)
-        
+        // ASSERT
         #expect(bankAccount.balance == 700)
     }
 }
