f611676f3ca9bdd1ce00d865596313f4bc3b2897
Jason <sanchezjason@me.com>
2025-04-28 09:41:32 -0500

    aprServiceTest

--- BluBank/BluBank/Services/APRService.swift
+++ BluBank/BluBank/Services/APRService.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+enum CreditScoreServiceError: LocalizedError {
+    case noCreditScoreFound
+    
+    // Provide localized error desccriptions
+    var errorDescription: String? {
+        switch self {
+            case .noCreditScoreFound:
+                return NSLocalizedString(
+                    "No Credit score was found for the provided SSN.",
+                    comment: "Error message when no credit is found.")
+        }
+    }
+}
+
+struct APRService {
+    // 3rd Party Service
+    let creditScoreService: CreditScoreService
+    
+    func getAPR(ssn: String) async throws -> Double {
+        
+        guard let creditScore = try await creditScoreService.getCreditScore(ssn: ssn) else {
+            throw CreditScoreServiceError.noCreditScoreFound
+        }
+        if let score = creditScore.score {
+            if score > 650 {
+                return Double.random(in: 1...3)
+            } else {
+                return Double.random(in: 6...10)
+            }
+        } else {
+            throw CreditScoreServiceError.noCreditScoreFound
+        }
+    }
+}
--- BluBank/BluBankTests/APRServiceTests.swift
+++ BluBank/BluBankTests/APRServiceTests.swift
@@ -9,5 +9,14 @@ import Testing
 @testable import BluBank
 
 struct APRServiceTests {
-
+    @Test
+    func apr_is_within_expected_range_for_valid_ssn() async throws {
+        let ssn = "123-45-6789" // ssn with credit score > 650
+        let expectedAPRRange = 1.0...3.0
+        
+        let aprService = APRService(creditScoreService: CreditScoreService())
+        let apr = try await aprService.getAPR(ssn: ssn)
+        
+        #expect(expectedAPRRange.contains(apr))
+    }
 }
